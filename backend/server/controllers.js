0e01c341f796275b771b4ba9be1101b7cbe6ae35
my-name-is-ben <ben.cunningham112@gmail.com>
2021-12-22 19:25:30 -0700

    Removed commented-out API Token. Oops.

--- backend/server/controllers.js
+++ backend/server/controllers.js
@@ -1,9 +1,6 @@
 const axios = require('axios');
 const api_info = require('./config.js');
 
-// const API_KEY = process.env.REACT_APP_API_KEY;
-// axios.defaults.headers.common['Authorization'] = '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';
-
 const API_KEY = api_info.API_KEY;
 axios.defaults.headers.common['Authorization'] = API_KEY;
 
