02428b56782e8a47793e216682e52bec8834cde3
Liam Macfarlane <solacebrews@gmail.com>
2021-12-29 09:10:21 -0700

    built carousel, starting to work out asynq calls

---
+++ frontend/src/components/RelatedItems/OutfitData.jsx
@@ -0,0 +1,5 @@
+export const OutfitData = [
+  {
+    image: 'https://previews.123rf.com/images/maestriadiz/maestriadiz1203/maestriadiz120300001/12912916-maniqu%C3%ADes-en-una-posici%C3%B3n-diferente-sobre-un-fondo-blanco.jpg'
+  }
+]
--- frontend/src/components/RelatedItems/RelatedItemsCarousel.jsx
+++ frontend/src/components/RelatedItems/RelatedItemsCarousel.jsx
@@ -2,12 +2,28 @@ import React, {useState, useContext, useEffect} from 'react';
 import GlobalContext from '../../Contexts/index.jsx';
 import RelatedContext from './RelatedContext.jsx'
 import axios from 'axios';
-import RelatedCard from './RelatedCard.jsx';
+import { RelatedImages } from './RelatedImages.jsx'
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
 
-const RelatedItemsCarousel = () => {
+const RelatedItemsCarousel = ({ slides }) => {
+  const [relatedData, setRelatedData] = useState([])
+  const [otherItems, setOtherItems] = useState(0)
   const {currentItem} = useContext(GlobalContext);
   const {relatedItems} = useContext(RelatedContext);
   const {setRelatedItems} = useContext(RelatedContext);
+  const length = slides.length
+
+  const comboFetch = (productID) => {
+    const relatedObjects = [];
+    axios.get(`/api/products/${productID}/related`).then((res) => {
+      var relatedArray = res.data;
+      relatedArray.map((item, index) => {
+        axios.get(`/api/products/${item}/styles`).then((res) => {
+          relatedObjects.push(res.data);
+        })
+      })
+    })
+  }
 
   const fetchRelated = (productID) => {
     axios.get(`/api/products/${productID}/related`).then((res) => {
@@ -15,24 +31,64 @@ const RelatedItemsCarousel = () => {
     })
   };
 
+  const fetchRelatedstyles = (state) => {
+    const dataArray = [];
+    state.map((productID, index) => {
+    axios.get(`/api/products/${productID}/styles`).then((res) => {
+      dataArray.push(res.data);
+    });
+  });
+    setRelatedData(dataArray);
+    //console.log(relatedData)
+  }
 
   useEffect(() => {
       fetchRelated(currentItem.id);
-  }, [currentItem]);
+  }, [otherItems]);
+
+  useEffect(() => {
+    fetchRelatedstyles(relatedItems)
+  }, [relatedItems])
+
+  const nextSlide = () => {
+    setOtherItems(otherItems === length - 1 ? 0 : otherItems + 1)
+  };
+
+  const prevSlide = () => {
+    setOtherItems(otherItems === 0 ? length - 1 : otherItems - 1)
+  };
+
+  if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
 
   return (
-    <div>related items carousel
-      <div>items related to : {currentItem.name} </div>
-      <div>
-        {relatedItems.map((item, index) => {
+    <section className="relatedSlider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
+      {RelatedImages.map((slide, index) => {
+         return (
+           <div className={index === otherItems ? 'relatedSlideActive' : 'relatedSlide'} key={index}>
+             {index === otherItems && (<img src={slide.image} alt="loading image"
+              className="carouselImage" />
+              )}
+           </div>
+         )
+      })}
+    </section>
 
-          return (
-              <RelatedCard key={index} item={item}/>
-          )
-        })}
-      </div>
+    // <div>related items carousel
+    //   <div>items related to : {currentItem.name} </div>
+    //   <div>
+    //     {relatedItems.map((item, index) => {
 
-    </div>
+    //       return (
+    //           <RelatedCard key={index} item={item}/>
+    //       )
+    //     })}
+    //   </div>
+
+    // </div>
   );
 
 };
