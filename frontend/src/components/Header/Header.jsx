59ff09a6adc4eca2b980aae18e969db780ddbe5b
Neil Mosser <david.mosser7207@gmail.com>
2021-12-30 10:56:13 -0700

    Added some CSS to header, added dummy search bar, improved styling on overview dropdown menus

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import {FaSearch} from "react-icons/fa";
 
 const Header = () => {
   return (
     <div className="header">
         <img className="logo" src="./narsil_edit_2.png" />
         <h1 className="bilbosbaubles">Bilbo's Baubles</h1>
-      <div className="search">Search</div>
+        <div className="search-div">
+           <input type="text" placeholder="Search..." className="search"></input>
+           <FaSearch className="search-icon"/>
+        </div>
     </div>
   );
 };
--- frontend/src/components/Overview/ProductInformation/AddToCart.jsx
+++ frontend/src/components/Overview/ProductInformation/AddToCart.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import {FaRegStar, FaStar} from 'react-icons/fa';
 
 const AddToCart = () => {
   return (
     <div>
-      <select name="selectList" id="selectList">
+      <select name="selectList" className="selectList">
         <option value="option 1">Add to bag</option>
         <option value="option 2">Option 2</option>
       </select>
-      <label>
-        <input type="checkbox" />
+      <label className="overview-favorite">
+          <input className="overview-checkbox" type="checkbox" />
+          <FaStar />
        </label>
 
     </div>
