06e0e5d7b54dc11f43115496cc67a419da7adb47
Neil Mosser <david.mosser7207@gmail.com>
2022-01-04 09:12:06 -0700

    Refactored carousel, worked on getting size and quantity, updated css, changed checkboxes to show correct features

--- frontend/src/components/Overview/ProductInformation/OverviewTitle.jsx
+++ frontend/src/components/Overview/ProductInformation/OverviewTitle.jsx
@@ -1,16 +1,19 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import GlobalContext from '../../../Contexts/index.jsx';
 import OverviewContext from '../../../Contexts/OverviewContext.jsx';
 
 const OverviewTitle = () => {
   const {currentItem} = useContext(GlobalContext);
   const {getAllStyles, allStyles, setAllStyles} = useContext(OverviewContext);
+  const [price, setPrice] = useState(currentItem.default_price);
 
    useEffect(() => {
 
     getAllStyles();
 
   }, [currentItem]);
+
+
 
   return (
     <div className="overviewTitle">
@@ -20,5 +23,7 @@ const OverviewTitle = () => {
     </div>
   );
 };
+
+// currentItem.default_price
 
 export default OverviewTitle;
